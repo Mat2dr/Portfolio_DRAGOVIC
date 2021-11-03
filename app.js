7187336c28cf58db9c861523d56b00d033ad157a
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-11-03 22:05:03 +0100

    All animation JS

--- app.js
+++ app.js
@@ -1,14 +1,59 @@
 let controller;
-let slideScene;
+let aboutScene;
+let projectScene;
+let testimonialScene;
+let faqScene;
+let contactScene;
 
-function animateNav() {
+function animateHead() {
     controller = new ScrollMagic.Controller();
+
     const nav = document.querySelector('.navbar');
+    const img = document.querySelector('img');
+    const revealImg = document.querySelector(".reveal-img");
+    const title = document.querySelector('.hero__img__text');
+    const smalltitle = document.querySelector('.hero__titre');
+
 
     const navTl = gsap.timeline({
+        defaults: { duration: 2, ease: "power2.inOut" }
+    });
+    navTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, "=0.5");
+    navTl.fromTo(img, {opacity: 0}, { opacity: 1}, "-=1.5");
+    navTl.fromTo(title, {opacity: 0}, { opacity: 1}, "-=1.5");
+    navTl.fromTo(smalltitle, {opacity: 0}, { opacity: 1}, "-=1.5");
+    navTl.fromTo(revealImg, { x: '0%' }, { x: '100%' }, "-=0.5");
+};
+
+function animateCall() {
+    controller = new ScrollMagic.Controller();
+
+    const revealTextCall = document.querySelector(".reveal-text-call");
+
+    const callTl = gsap.timeline({
         defaults: { duration: 1, ease: "power2.inOut" }
     });
-    navTl.fromTo(nav, { y: '-100%' }, { y: '0%' } );
+
+    callTl.fromTo(revealTextCall, {opacity: 1}, { opacity: 0}, "=2.5");
+};
+
+function animateAbout() {
+    //init Controller
+    controller = new ScrollMagic.Controller();
+    //Select some things
+    const about = document.querySelectorAll('.about');
+    const aboutTl = gsap.timeline({
+        defaults: { duration: 1, ease: "power2.inOut" }
+    });
+    aboutTl.fromTo(about, {opacity: 0}, { opacity: 1}, "-=0.75");
+    //create Scene
+    aboutScene = new ScrollMagic.Scene({
+        triggerElement: about,
+        triggerHook: 0.60,
+        reverse: false
+    })
+        .setTween(aboutTl)
+    .addTo(controller);
 };
 
 function animateProject() {
@@ -20,14 +65,90 @@ function animateProject() {
     projects.forEach(project =>{
         const revealImg = project.querySelector(".reveal-img");
         const img = project.querySelector('img');
+        const revealText = project.querySelector(".reveal-text");
+        const btn = project.querySelectorAll(".btn");
         //GSAP
         const mainTl = gsap.timeline({
             defaults: { duration: 1, ease: "power2.inOut" }
         });
         mainTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+        mainTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, "-=1");
         mainTl.fromTo(revealImg, { x: '0%' }, { x: '100%' }, "-=0.75");
+        mainTl.fromTo(btn, {opacity: 0}, { opacity: 1}, "-=0.75");
+        //create Scene
+        projectScene = new ScrollMagic.Scene({
+            triggerElement: project,
+            triggerHook: 0.60,
+            reverse: false
+        })
+            .setTween(mainTl)
+        .addTo(controller);
     });
 };
 
-animateNav();
+function animateTestimonial() {
+    //init Controller
+    controller = new ScrollMagic.Controller();
+    //Select some things
+    const testimonial = document.querySelectorAll('.testimonial-text');
+    const testimonialTl = gsap.timeline({
+        defaults: { duration: 1, ease: "power2.inOut" }
+    });
+    testimonialTl.fromTo(testimonial, {opacity: 0}, { opacity: 1}, "-=0.75");
+    //create Scene
+    aboutScene = new ScrollMagic.Scene({
+        triggerElement: testimonial,
+        triggerHook: 0.60,
+        reverse: false
+    })
+        .setTween(testimonialTl)
+    .addTo(controller);
+};
+
+
+function animateFaq() {
+    //init Controller
+    controller = new ScrollMagic.Controller();
+    //Select some things
+    const faq = document.querySelectorAll('.faq');
+    const faqTl = gsap.timeline({
+        defaults: { duration: 1, ease: "power2.inOut" }
+    });
+    faqTl.fromTo(faq, {opacity: 0}, { opacity: 1}, "-=0.8");
+    //create Scene
+    faqScene = new ScrollMagic.Scene({
+        triggerElement: faq,
+        triggerHook: 0.30,
+        reverse: false
+    })
+        .setTween(faqTl)
+    .addTo(controller);
+};
+
+
+function animateContact() {
+    //init Controller
+    controller = new ScrollMagic.Controller();
+    //Select some things
+    const contact = document.querySelectorAll('.contacter');
+    const contactTl = gsap.timeline({
+        defaults: { duration: 1, ease: "power2.inOut" }
+    });
+    contactTl.fromTo(contact, {opacity: 0}, { opacity: 1}, "-=0.75");
+    //create Scene
+    contactScene = new ScrollMagic.Scene({
+        triggerElement: contact,
+        triggerHook: 0.60,
+        reverse: false
+    })
+        .setTween(contactTl)
+    .addTo(controller);
+};
+
+animateHead();
+animateCall();
+animateAbout();
 animateProject();
+animateTestimonial();
+animateFaq();
+animateContact();
